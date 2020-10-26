032e8042c4a299fe98766deb2f4f073320888b5d
Claire DeBoer <claire.deboer@gmail.com>
2020-10-26 19:13:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,8 +1,10 @@
-def create_an_empty_array []
+def create_an_empty_array 
+  []
   
 end
 
-def create_an_array <<"New York" <<"Chicago" <<"Paris" <<"Milwaukee"
+def create_an_array 
+["New York", "Chicago", "Paris", "Milwaukee"]
   
 end
 
