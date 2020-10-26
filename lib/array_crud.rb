8a20901db021a33620e171645c3afdc83524a812
Claire DeBoer <claire.deboer@gmail.com>
2020-10-26 19:23:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,12 +9,12 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push("San Francisco")
+  array.push(element)
   
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift("Oakland")
+  array.unshift(element)
   
 end
 
@@ -28,17 +28,17 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[array.length-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array[index_number] = element
 end
